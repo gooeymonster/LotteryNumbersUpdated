ac6cdab224c2752b4b60c2e9ffa71bb9fa65ae1c
Daniel Ali <32986138+gooeymonster@users.noreply.github.com>
2019-05-30 15:53:14 -0500

    Add files via upload

---
+++ LotteryNumbers.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class LotteryNumbers {
+    private ArrayList<Integer> numbers;
+    private Random random = new Random();
+    private ArrayList<Integer> numbersMain;
+
+    public LotteryNumbers() {
+        // Draw numbers as LotteryNumbers is created
+
+        this.drawNumbers();
+    }
+
+    public ArrayList<Integer> numbers() {
+        return this.numbers;
+    }
+
+    public void drawNumbers() {
+        // We'll format a list for the numbers
+        this.numbers = new ArrayList<Integer>();
+        this.numbersMain = new ArrayList<Integer>();
+        // Write the number drawing here using the method containsNumber()
+        int counter = 0;
+        while(counter<7) {
+            int bound = 1 * this.random.nextInt(39) + 1;
+            if (this.containsNumber(bound)) {
+                this.numbers.add(1 * this.random.nextInt(39 ) + 1);
+            }
+            else{
+                this.numbers.add(bound);
+            }
+            counter++;
+        }
+        /**
+         * int i = 1;
+        int counter = 0;
+        while(counter<39){
+            this.numbersMain.add(i);
+            i++;
+            counter++;
+        }
+         **/
+        /**
+        int counter2 = 0;
+
+        while(counter2<7){
+            if(!this.containsNumber(numbersMain.size())){
+                this.numbers.add((this.random.nextInt(numbersMain.size())));
+            }
+            else{
+
+            }
+            counter2++;
+        }
+         **/
+    }
+
+    public boolean containsNumber(int number) {
+        // Test here if the number is already in the drawn numbers
+        if(this.numbers.contains(number)){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+}
